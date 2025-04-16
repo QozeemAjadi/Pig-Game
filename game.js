ee388c3be80a1dc701e4be4aaf01843de8a4150a
QozeemAjadi <qozeemahmed01@gmail.com>
2025-04-16 10:43:47 +0100

    updated

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
  var scores, roundScore, activePlayer;
  scores = [0,0];
  roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 
 
@@ -20,6 +20,24 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
      diceDOM.style.display = 'block';
      diceDOM.src = 'dice-' + dice + '.png';
 
+
+//3. update the round score If the the rolled nunmber was NOT a 1
+    if (dice !== 1){
+      //add score
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore; 
+
+    } else{
+      //next player
+       activePlayer === 0 ? activePlayer= 1 : activePlayer = 0;
+       roundScore =0;  
+
+       document.getElementById('current-0').textContent = '0';
+       document.getElementById('current-1').textContent = '0';
+
+       document.querySelector('.player-0-panel').textContent.remove('active');
+       document.querySelector('.player-0-panel').textContent.remove('active'); 
+    }
 });
 
 
